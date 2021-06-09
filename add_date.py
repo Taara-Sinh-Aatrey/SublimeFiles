b0d6c704859e7d3075139402d55b5ca59a80d611
Taara-Sinh-Aatrey <taarasinh1@gmail.com>
2021-06-09 08:13:07 +0530

    Added Files

---
+++ add_date.py
@@ -0,0 +1,7 @@
+import datetime, getpass
+import sublime, sublime_plugin
+import os
+
+class AddDateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  "/**\n *    author:  Taara Sinh Aatrey\n *    created: " + datetime.date.today().strftime("%d.%m.%Y") + " " + datetime.datetime.now().strftime("%H:%M:%S") + "\n**/\n\n"} )
